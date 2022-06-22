db74ba3e53499cc6e96210bd9f24c115618e1323
Rehan Goraya <72587992+Rehan-05@users.noreply.github.com>
2022-06-23 01:49:48 +0500

    new updation

--- config/auth.config.js
+++ config/auth.config.js
@@ -1,9 +1,5 @@
 module .exports ={
 secret:"Reh-031678",
 Recent_Password:"Account_Activate_key_123456",
-SMTP_USER:'laibaabbasi857@gmail.com',
-SMTP_PASSWORD:'RehanGoraya123!?',
-SMPT_PORT:'3000',
-SMTP_HOST:'smtp.gmail.com',
 BASE_URL : "http://localhost:3000",
 }
